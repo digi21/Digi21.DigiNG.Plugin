34fb58d0f5914ae87cefb003e05f4f34903bb90b
José Ángel Martínez Torres <joseangelmt@outlook.com>
2019-11-18 16:34:09 +0100

    Updated assembly information and strong naming

--- Digi21.DigiNG.Plugin/Properties/AssemblyInfo.cs
+++ Digi21.DigiNG.Plugin/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: ReferenceAssembly]
 [assembly: AssemblyTitle("Digi21.DigiNG.Plugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains the types for programming DigiNG plugins in Digi3D.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dreaming With Objects S.L.")]
 [assembly: AssemblyProduct("Digi21.DigiNG.Plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright (c) 2010-2019 Digi21.net")]
+[assembly: AssemblyTrademark("(c) 2010-2019 José Ángel Martínez Torres")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("17.2.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
