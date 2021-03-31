5b04fc9ee2e50c63833ae87ebfa0429ccb421c67
José Ángel <joseangelmt@outlook.com>
2021-03-31 18:00:29 +0200

    Version 17.3.0

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe pack Digi21.DigiNG.Plugin.nuspec
-nuget.exe sign Digi21.DigiNG.Plugin.17.2.1.nupkg -CertificatePath dwo.pfx -TimeStamper http://tsa.starfieldtech.com
+nuget.exe sign Digi21.DigiNG.Plugin.17.3.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://tsa.starfieldtech.com
