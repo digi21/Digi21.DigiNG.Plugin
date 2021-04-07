b9c5e3801288589074c7e440d112f32e416ec548
José Ángel <joseangelmt@outlook.com>
2021-04-07 11:55:39 +0200

    Importado TaskMessageBox de digi21/Digi21.DigiNG

---
+++ Digi21.DigiNG.Plugin/DigiNG/Plugin/TaskPanel/TaskMessageBox.cs
@@ -0,0 +1,12 @@
+﻿using Digi21.Digi3D;
+
+namespace Digi21.DigiNG.Plugin.TaskPanel
+{
+    public sealed class TaskMessageBox : TaskBase
+    {
+        public TaskMessageBox() => throw null;
+        public TaskMessageBox(string title, string description, TaskSeverity severity, string drawingFile, string module) => throw null;
+        public TaskMessageBox(string title, string description, TaskSeverity severity, string drawingFile, string module, ITask[] childs) => throw null;
+        public override void Execute() => throw null;
+    }
+}
