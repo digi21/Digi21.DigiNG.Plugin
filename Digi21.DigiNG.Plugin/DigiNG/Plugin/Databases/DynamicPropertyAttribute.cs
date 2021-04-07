8f4de870b49925c59c05792daa7da55be53b731b
José Ángel <joseangelmt@outlook.com>
2021-04-07 10:23:29 +0200

    Added Database extension types

---
+++ Digi21.DigiNG.Plugin/DigiNG/Plugin/Databases/DynamicPropertyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Digi21.DigiNG.Plugin.Search
+{
+    public class DynamicPropertyAttribute : Attribute
+    {
+        public DynamicPropertyAttribute() => throw null;
+        public DynamicPropertyAttribute(string dynamicProperty) => throw null;
+
+        public string DynamicProperty { get => throw null; set => throw null; }
+    }
+}
+
+
---
+++ Digi21.DigiNG.Plugin/DigiNG/Plugin/Databases/IDynamicProperty.cs
@@ -0,0 +1,9 @@
+﻿using Digi21.DigiNG.Entities;
+
+namespace Digi21.DigiNG.Plugin.Search
+{
+    public interface IDynamicProperty
+    {
+        object Transform(string parameter, Entity entity);
+    }
+}
