2a794cea01c59d3bb6c3c38c214a824ca080905a
José Ángel <joseangelmt@outlook.com>
2021-05-24 18:07:44 +0200

    Added overloads to Panes.Add

--- Digi21.DigiNG.Plugin/Digi3D/Panes.cs
+++ Digi21.DigiNG.Plugin/Digi3D/Panes.cs
@@ -1,11 +1,15 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Digi21.Digi3D
 {
     public class Panes
     {
         public Pane Add(Form form) => throw null;
+        public Pane Add(Form form, Size size) => throw null;
         public Pane Add(Form form, PaneDock initialDock) => throw null;
+        public Pane Add(Form form, Size size, PaneDock initialDock) => throw null;
         public Pane Add(Form form, PaneDock initialDock, bool allowClose) => throw null;
+        public Pane Add(Form form, Size size, PaneDock initialDock, bool allowClose) => throw null;
     }
 }
