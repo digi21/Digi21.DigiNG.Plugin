c80f6df77febd9cbea5ebff2ee6df0f9f2b07331
José Ángel <joseangelmt@outlook.com>
2021-05-28 13:00:45 +0200

    Added overloads to Panes.Add. digi21/TareasDigi3D#373

--- Digi21.DigiNG.Plugin/Digi3D/Panes.cs
+++ Digi21.DigiNG.Plugin/Digi3D/Panes.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
 namespace Digi21.Digi3D
@@ -11,5 +12,13 @@ namespace Digi21.Digi3D
         public Pane Add(Form form, Size size, PaneDock initialDock) => throw null;
         public Pane Add(Form form, PaneDock initialDock, bool allowClose) => throw null;
         public Pane Add(Form form, Size size, PaneDock initialDock, bool allowClose) => throw null;
+
+        public T Add<T>(Form form, params object[] args) where T : Pane => throw null;
+        public T Add<T>(Form form, Size size, params object[] args) where T : Pane => throw null;
+        public T Add<T>(Form form, PaneDock initialDock, params object[] args) where T : Pane => throw null;
+        public T Add<T>(Form form, Size size, PaneDock initialDock, params object[] args) where T : Pane => throw null;
+        public T Add<T>(Form form, PaneDock initialDock, bool allowClose, params object[] args) where T : Pane => throw null;
+        public T Add<T>(Form form, Size size, PaneDock initialDock, bool allowClose, params object[] args) where T : Pane => throw null;
+
     }
 }
