692ba75288eb97db666afd99aa91a8a71c5f976f
José Ángel <joseangelmt@outlook.com>
2021-10-23 08:29:30 +0200

    Changed signature of AnalyzeEntity

--- Digi21.DigiNG.Plugin/DigiNG/Plugin/QualityControl/ITrigger.cs
+++ Digi21.DigiNG.Plugin/DigiNG/Plugin/QualityControl/ITrigger.cs
@@ -1,10 +1,9 @@
 ﻿using Digi21.DigiNG.Entities;
-using System.Collections.Generic;
 
 namespace Digi21.DigiNG.Plugin.QualityControl
 {
     public interface ITrigger
     {
-        Entity AnalyzeEntity(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes, bool addingEntity);
+        Entity AnalyzeEntity(Entity entity, bool addingEntity);
     }
 }
