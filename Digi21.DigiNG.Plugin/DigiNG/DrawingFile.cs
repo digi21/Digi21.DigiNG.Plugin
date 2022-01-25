9e7ddb75441a65349d5c0432046f62dd2b045147
José Ángel <joseangelmt@outlook.com>
2022-01-25 14:08:50 +0100

    Added method IndexOf. digi21/TareasDigi3D#455

--- Digi21.DigiNG.Plugin/DigiNG/DrawingFile.cs
+++ Digi21.DigiNG.Plugin/DigiNG/DrawingFile.cs
@@ -34,6 +34,7 @@ namespace Digi21.DigiNG
         public bool CanRead => throw null;
         public string Path => throw null;
         public string ConnectionString => throw null;
+        public int IndexOf(Entity entity) => throw null;
 
         #region IWindow3D
         public Point3D W => throw null;
