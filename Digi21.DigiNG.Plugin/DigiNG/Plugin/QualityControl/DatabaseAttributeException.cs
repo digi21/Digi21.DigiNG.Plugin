a64d0ad96108ad1a44c2097e1c5552b40a342226
José Ángel <joseangelmt@outlook.com>
2022-03-25 10:32:26 +0100

    Updated DatabaseAttributeException digi21/TareasDigi3D#502

--- Digi21.DigiNG.Plugin/DigiNG/Plugin/QualityControl/DatabaseAttributeException.cs
+++ Digi21.DigiNG.Plugin/DigiNG/Plugin/QualityControl/DatabaseAttributeException.cs
@@ -1,16 +1,15 @@
-﻿using Digi21.DigiNG.Entities;
-using System;
+﻿using System;
 
 namespace Digi21.DigiNG.Plugin.QualityControl
 {
     public class DatabaseAttributeException : Exception
     {
-        public DatabaseAttributeException(string message, Code code, string field) => throw null;
-        public DatabaseAttributeException(string message, Code code, string field, Exception innerException) => throw null;
-        public DatabaseAttributeException(string message, Code code, string field, Tuple<string, string>[] contextualMenuOptions) => throw null;
-        public DatabaseAttributeException(string message, Code code, string field, Exception innerException, Tuple<string, string>[] contextualMenuOptions) => throw null;
+        public DatabaseAttributeException(string message, int codeIndex, string field) => throw null;
+        public DatabaseAttributeException(string message, int codeIndex, string field, Exception innerException) => throw null;
+        public DatabaseAttributeException(string message, int codeIndex, string field, Tuple<string, string>[] contextualMenuOptions) => throw null;
+        public DatabaseAttributeException(string message, int codeIndex, string field, Exception innerException, Tuple<string, string>[] contextualMenuOptions) => throw null;
         public Tuple<string, string>[] ContextualMenuOptions { get => throw null; set => throw null; }
         public string Field { get => throw null; set => throw null; }
-        public Code Code { get => throw null; set => throw null; }
+        public int CodeIndex { get => throw null; set => throw null; }
     }
 }
