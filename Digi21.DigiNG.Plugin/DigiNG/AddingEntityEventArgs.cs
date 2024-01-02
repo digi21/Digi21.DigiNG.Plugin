1eda401013a6502d9eefd8456e7a8bb6cc37a0d4
José Ángel <joseangelmt@outlook.com>
2024-01-02 15:51:34 +0100

    Eliminado diccionario de atributos

--- Digi21.DigiNG.Plugin/DigiNG/AddingEntityEventArgs.cs
+++ Digi21.DigiNG.Plugin/DigiNG/AddingEntityEventArgs.cs
@@ -6,9 +6,8 @@ namespace Digi21.DigiNG
 {
     public class AddingEntityEventArgs : EventArgs
     {
-        public AddingEntityEventArgs(Entity entity, Dictionary<string, IDictionary<string, object>> databaseAttributes) => throw null;
+        public AddingEntityEventArgs(Entity entity) => throw null;
 
-        public Dictionary<string, IDictionary<string, object>> DatabaseAttributes => throw null;
         public bool Cancel { get; set; }
         public Entity Entity => throw null;
     }
